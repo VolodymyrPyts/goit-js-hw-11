1462b35518059600ef8fdba23c1b012286b75f37
VolodymyrPyts <volodymyr.pyts@gmail.com>
2022-07-04 11:55:37 +0300

    js project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 
 import NewsApiServes from "./js/new-serves";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+Notify.init({
+width: '300px',
+position: 'rigth-top',
+timeout: 1000
+});
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
     searchBtn: document.querySelector('button'),
-    loadMoreBtn: document.querySelector('.load-more'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  galleryBlock: document.querySelector('.gallery')
 }
 
 
@@ -12,34 +22,87 @@ refs.searchForm.addEventListener('submit', onButtonClick);
 refs.loadMoreBtn.addEventListener('click', onLoadMore)
 const newsApiServes = new NewsApiServes();
 
-function onButtonClick(e) {
-    e.preventDefault();
-    newsApiServes.page = 1; 
-    newsApiServes.query = e.currentTarget.elements.searchQuery.value
-     newsApiServes.fetchArticles()
+async function onButtonClick(e) {
+  e.preventDefault();
+  clearGelleryForm()
+  newsApiServes.page = 1; 
+ 
+
+    newsApiServes.query = e.target.elements.searchQuery.value.trim()
+console.log(newsApiServes.query) 
+  if (!newsApiServes.query) { return Notify.info("Еnter a search query") }
+  try {
+    const { hits, totalHits } = await newsApiServes.fetchArticles()
+     if(!totalHits) return Notify.warning("Sorry, there are no images matching your search query. Please try again.")
+    randerGallary(hits);
+    lightbox.refresh();
+Notify.success(`Hooray! We found ${totalHits} images.`)
+  refs.searchForm.reset();
+  }
+  catch (error) { caonsole.log(error)}  
+ 
+  
+  
+  }
+
+async function onLoadMore() {
+  try {
+    const { hits, totalHits } = await newsApiServes.fetchArticles()
+    randerGallary(hits)
+    lightbox.refresh();
+    scrollSmoothly()
+  } catch (error) { caonsole.log(error)} 
+   
 }
 
-function onLoadMore () {
-  newsApiServes.fetchArticles()  
+ function makeGalleryMakup(searchImage) {
+   return searchImage.map(({ largeImageURL,
+     webformatURL,
+     tags,
+     likes,
+     views,
+     comments,
+     downloads }) => `
+    <div class="photo-card">
+    <a href = "${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+  </a>
+</div>`).join('');
 }
 
+function randerGallary(searchImg) {
+    refs.galleryBlock.insertAdjacentHTML('beforeend', makeGalleryMakup(searchImg))
+}
 
+function clearGelleryForm() {
+  refs.galleryBlock.innerHTML = "";
+}
 
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 500,
+});
+function scrollSmoothly() {
+  const { height: cardHeight } = document
+    .querySelector(".gallery")
+    .firstElementChild.getBoundingClientRect();
 
-// .<div class="photo-card">
-//   <img src="" alt="" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//     </p>
-//   </div>
-// </div>
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: "smooth",
+  });
+}
--- src/js/new-serves.js
+++ src/js/new-serves.js
@@ -5,22 +5,22 @@ export default class NewsApiServes {
         this.page = 1;
     }
     
-    fetchArticles() {
-const url = `https://pixabay.com/api/?key=28351441-36643387bd1a8158570da0675&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}`
-    
-        axios.get (url
-    )
-            .then(r => {
-                this.page += 1;
-                console.log(r.data.hits)
-                
-            }
-            )
-    }
+   async fetchArticles() {
+const url = `https://pixabay.com/api/?key=28351441-36643387bd1a8158570da0675&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
+    this.icrementPage()
+       const {data} = await axios.get (url
+       )
+       return data;
+        
+    } 
     get query() {
         return this.searchQuery;
+        
     }
     set query(newSearchQuery) {
         this.searchQuery = newSearchQuery;   
+    }
+    icrementPage() {
+        this.page += 1;
     }
 }
